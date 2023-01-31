78d3f91c19207d8efc11cc5b9da6345c0fff8f32
parikshit456 <parikshit.agarwal910@gmail.com>
2023-01-31 17:57:52 +0530

    feat: added feature to translate

---
+++ app.js
@@ -0,0 +1,30 @@
+var btnTranslate = document.querySelector("#btn");
+var textArea = document.querySelector("#text-area")
+var output = document.querySelector(".output-container");
+
+var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=";
+
+function getTranslatedUrl(text){
+    return serverUrl+"?"+"text="+text;
+}
+
+
+btnTranslate.addEventListener("click",function clickEventHandler(){
+    // output.innerText = "Translated:"+textArea.value
+    // console.log("Clicked");
+    // console.log(textArea.value);
+
+    var inputTxt = textArea.value;
+    // output.inner
+
+    fetch(getTranslatedUrl(inputTxt)).then(response => response.json()).then(json =>{
+        
+        translatedText=json.contents.translation
+        console.log(translatedText);
+        output.textContent = translatedText;
+    } );
+    
+
+})
+
+
